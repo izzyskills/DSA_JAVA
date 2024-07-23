e723aadf5f2f9a272147265601047464f3f9c6d7
Israel Omola <izzyskills@gmail.com>
2024-07-23 21:20:02 +0100

    added linked list insertion

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,38 @@
 package DSA;
 
 public class LinkedList<T> {
-    private T value;
-    private LinkedList<T> parent;
-    private LinkedList<T> children;
+    Node<T> head;
+
+    static class Node<T> {
+        T data;
+        Node<T> next, prev;
+
+        Node(T d) {
+            data = d;
+            next = prev = null;
+        }
+
+    }
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public void insert(T data) {
+        Node<T> new_node = new Node<T>(data);
+
+        if (head == null) {
+            head = new_node;
+
+        } else {
+            Node<T> last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            last.next = new_node;
+            last.next.prev = last;
+        }
+
+    }
 }
