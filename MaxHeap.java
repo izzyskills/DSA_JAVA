1c74ac6a37f18060e426b0e71aed24099c290e0c
Israel Omola <izzyskills@gmail.com>
2024-07-28 11:20:34 +0100

    heap

--- MaxHeap.java
+++ MaxHeap.java
@@ -46,4 +46,58 @@ public class MaxHeap {
         return (2 * i + 2);
     }
 
+    int removeMax() {
+        // Checking whether the heap array
+        // is empty or not.
+        if (heapSize <= 0)
+            return Integer.MIN_VALUE;
+        if (heapSize == 1) {
+            heapSize--;
+            return arr[0];
+        }
+
+        // Storing the maximum element
+        // to remove it.
+        int root = arr[0];
+        arr[0] = arr[heapSize - 1];
+        heapSize--;
+
+        // To restore the property
+        // of the Max heap.
+        MaxHeapify(0);
+
+        return root;
+    }
+
+    // Increases value of key at
+    // index 'i' to new_val.
+    void increaseKey(int i, int newVal) {
+        arr[i] = newVal;
+        while (i != 0 && arr[parent(i)] < arr[i]) {
+            int temp = arr[i];
+            arr[i] = arr[parent(i)];
+            arr[parent(i)] = temp;
+            i = parent(i);
+        }
+    }
+
+    // Returns the maximum key
+    // (key at root) from max heap.
+    int getMax() {
+        return arr[0];
+    }
+
+    int curSize() {
+        return heapSize;
+    }
+
+    // Deletes a key at given index i.
+    void deleteKey(int i) {
+        // It increases the value of the key
+        // to infinity and then removes
+        // the maximum value.
+        increaseKey(i, Integer.MAX_VALUE);
+        removeMax();
+    }
+
 }
